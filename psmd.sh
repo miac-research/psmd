a2d0d4b47ec8dc8aeda412edc31a11cda3dfcf93
Marco Duering <marco.duering@miac.ch>
2020-11-09 12:50:48 +0100

    v1.5
    Version 1.5 (2019)

--- psmd.sh
+++ psmd.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# shellcheck disable=SC2166,SC2015
 #
-# PSMD processing pipeline, v 1.0 (first release 2016-08)
+# FOR MORE INFORMATION, PLEASE VISIT: http://www.psmd-marker.com
+#
+# PSMD processing pipeline, v 1.5 (2019)
 #
 # This script is provided under the revised BSD (3-clause) license
 #
-# Copyright (c) 2016, Institute for Stroke and Dementia Research, Munich
+# Copyright (c) 2016, 2019, Institute for Stroke and Dementia Research, Munich
 # http://www.isd-muc.de  All rights reserved. 
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -37,41 +40,48 @@
 # Jouvent E, Alzheimer’s Disease Neuroimaging Initiative (ADNI), Chabriat H, 
 # Ertl-Wagner B, Ewers M, Schmidt R, de Leeuw FE, Biessels GJ, Dichgans M, Duering M
 # A novel imaging marker for small vessel disease based on skeletonization of white 
-# matter tracts and diffusion histograms. Annals of Neurology 2016 (in press)
+# matter tracts and diffusion histograms. Annals of Neurology 2016, 80(4):581-92
+# DOI: 10.1002/ana.24758
 # 
 # For FSL-TBSS:
 # Follow the guidelines at http://fsl.fmrib.ox.ac.uk/fsl/fslwiki/TBSS
-# 
-
-
-# Functions
 
 usage(){
 echo ""
+echo "PSMD - Peak width of Skeletonized Mean Diffusivity - pipeline version 1.5 (2019)"
+echo "http://www.psmd-marker.com"
+echo ""
 echo "Usage:"
 echo ""
-echo "  From unprocessed DTI data (recommended):"
-echo "  psmd.sh -d DTI-data -b bvals -r bvecs -s skeleton_mask"
+echo "  From unprocessed DWI data (recommended):"
+echo "  psmd.sh -d <DWI_data> -b <bvals> -r <bvecs> -s <skeleton_mask>"
 echo ""
-echo "    DTI-data: Unprocessed DTI data, after conversion from DICOM to Nifti"
-echo "    bvals: Text file containing b-values (created during DICOM conversion)"
-echo "    bvecs: Text file containing diffusion vectors (created during DICOM conversion)"
-echo "    skeleton_mask: Mask file provided with the psmd script (or custom mask)"
+echo "    -d <DWI_data>      Unprocessed DWI data in Nifti format"
+echo "    -b <bvals>         Text file containing b-values (in FSL format)"
+echo "    -r <bvecs>         Text file containing diffusion vectors (in FSL format)"
+echo "    -s <skeleton_mask> Skeleton mask file, e.g. the mask provided with the PSMD tool"
 echo ""
+echo "  From processed (corrected, brain extracted) DTI data (FA and MD) (NOT recommended):"
+echo "  psmd.sh -f <FA_image> -m <MD_image> -s <skeleton_mask>"
 echo ""
-echo "  From processed DTI data, i.e. FA and MD images (NOT recommended):"
-echo "  psmd.sh -f FA-image -m MD-image -s skeleton_mask"
+echo "    -f <FA_image>      The fractional anisotropy image, brain extracted, Nifti format"
+echo "    -m <MD_image>      The mean diffusivity image, brain extracted, Nifti format"
+echo "    -s <skeleton_mask> Skeleton mask file, e.g. the mask provided with the PSMD tool"
 echo ""
-echo "    FA-image: The fractional anisotropy image, brain extracted, Nifti format"
-echo "    MD-image: The mean diffusivity image, brain extracted, Nifti format"
-echo "    skeleton_mask: Mask file provided with the psmd script (or custom mask)"
+echo "  Options (non-mandatory):"
+echo "    -e <bvalue>        Enhanced masking of CSF and hyperintense voxels (e.g. certain artefacts)"
+echo "                       Please specify <b-value> of the diffusion shell to use (usually 1000)"
+echo "                       (only possible from unprocessed DWI data)"
+echo "    -l <lesion_mask>   Supply custom lesion mask in order to exclude a region from analysis"
 echo ""
-echo "  Options (nonmandatory):"
-echo "    -q    quiet operation (no status messages are displayed, only PSMD result)"
-echo "    -v    verbose (very detailed status and error messages are displayed)"
-echo "    -t    troubleshooting/debug (don't delete temporary files)"
+echo "    -o  Output mean skeletonized mean diffusivity (MSMD) instead of PSMD"
 echo ""
-exit
+echo "    -c  Clear temporary psmdtemp folder from previous run (if present)"
+echo "    -q  Quiet: No messages are displayed, only result (suitable for writing result into file)"
+echo "    -v  Verbose: Very detailed status and error messages are displayed"
+echo "    -t  Temporary files (folder psmdtemp) will not be deleted (for troubleshooting)"
+echo ""
+exit 1
 }
 
 # Function for absolute file names
@@ -84,7 +94,7 @@ get_abs_filename() {
 
 # Function for reporting level (redirect stdout/stderr)
 redirect_cmd() {
-    if [ $verbose == true ]; then
+    if [ "$verbose" == true ]; then
         "$@"
     else
         "$@" > /dev/null 2>&1
@@ -94,50 +104,70 @@ redirect_cmd() {
 # Check options
 [[ $# -eq 0 ]] && usage 
 
-#Check for FSL
-[ -z "${FSLDIR}" ] && { echo ""; echo "ERROR: This script requires a working installation of FSL 5"; echo "Please see http://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FslInstallation";echo ""; exit 1; }
+# Check for FSL
+[ -z "${FSLDIR}" ] && { echo ""; echo "ERROR: This script requires a working installation of FSL 5 or newer"; echo "Please see http://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FslInstallation";echo ""; exit 1; }
 
-# Empty/reset variables
-mask=
-dtiraw=
-bval=
-bvec=
-faimage=
-mdimage=
+# Check for dc
+command -v dc > /dev/null || { echo ""; echo "ERROR: This script requires the program 'dc' to work."; echo "Please install dc, e.g. in Ubuntu Linux via 'apt install dc'";echo ""; exit 1; }
+
+# Set default values for variables
+unset mask dtiraw bval bvec faimage mdimage enhmasb
+metric=PSMD
+enhmask=false
+lesionmasking=false
+cleartemp=false
 silent=false
 verbose=false
 debug=false
 basedir=$(pwd)
 
 # Get command-line options
-while getopts ":d:b:r:f:m:s:tqvh" opt; do
+while getopts ":d:b:r:f:m:s:e:l:otcqvh" opt; do
   case $opt in
     d)
       dtiraw=${OPTARG}
-      [ -r $dtiraw ] && dtirawfile=$(get_abs_filename "$dtiraw") || { echo ""; echo "ERROR: DTI raw file not found";echo ""; exit 1; }
+      [ -r "$dtiraw" ] && dtirawfile=$(get_abs_filename "$dtiraw") || { echo ""; echo "ERROR: DWI data file not found";echo ""; exit 1; }
       ;;
 	b)
       bval=${OPTARG}
-      [ -r $bval ] && bvalfile=$(get_abs_filename "$bval") || { echo ""; echo "ERROR: Bval file not found";echo ""; exit 1; }
+      [ -r "$bval" ] && bvalfile=$(get_abs_filename "$bval") || { echo ""; echo "ERROR: Bval file not found";echo ""; exit 1; }
       ;;
     r)
       bvec=${OPTARG}
-      [ -r $bvec ] && bvecfile=$(get_abs_filename "$bvec") || { echo ""; echo "ERROR: Bvec file not found";echo ""; exit 1; }
+      [ -r "$bvec" ] && bvecfile=$(get_abs_filename "$bvec") || { echo ""; echo "ERROR: Bvec file not found";echo ""; exit 1; }
       ;;
     f)
       faimage=${OPTARG}
-      [ -r $faimage ] && faimagefile=$(get_abs_filename "$faimage") || { echo ""; echo "ERROR: FA image file not found";echo ""; exit 1; }
+      [ -r "$faimage" ] && faimagefile=$(get_abs_filename "$faimage") || { echo ""; echo "ERROR: FA image file not found";echo ""; exit 1; }
       ;;
     m)
       mdimage=${OPTARG}
-      [ -r $mdimage ] && mdimagefile=$(get_abs_filename "$mdimage") || { echo ""; echo "ERROR: MD image file not found";echo ""; exit 1; }
+      [ -r "$mdimage" ] && mdimagefile=$(get_abs_filename "$mdimage") || { echo ""; echo "ERROR: MD image file not found";echo ""; exit 1; }
       ;;
     s)
       mask=${OPTARG}
-      [ -r $mask ] && maskfile=$(get_abs_filename "$mask") || { echo ""; echo "ERROR: Skeleton_mask file not found";echo ""; exit 1; }
+      [ -r "$mask" ] && maskfile=$(get_abs_filename "$mask") || { echo ""; echo "ERROR: Skeleton_mask file not found";echo ""; exit 1; }
+      ;;
+    e)
+      enhmask=true
+      enhmasb=${OPTARG}
+      [ -z "$bval" ] && { echo ""; echo "ERROR: Enhanced masking only possible when using unprocessed data as input.";echo ""; exit 1; }
+      bcheck=$(grep "$enhmasb" "$bvalfile")
+      [ -z "$bcheck" ] && { echo ""; echo "ERROR: Specified b-value (for enhanced masking) not found"; echo ""; exit 1; }
+      ;;
+    l)
+      lesionmasking=true
+      lesionmask=${OPTARG}
+      [ -r "$lesionmask" ] && lesionmaskfile=$(get_abs_filename "$lesionmask") || { echo ""; echo "ERROR: Lesion_mask file not found";echo ""; exit 1; }
+      ;;
+    o)
+      metric=MSMD
       ;;
     t)
       debug=true
+      ;;
+    c)
+      cleartemp=true
       ;;
     q)
       silent=true
@@ -151,82 +181,149 @@ while getopts ":d:b:r:f:m:s:tqvh" opt; do
     \?)
       echo ""
       echo "ERROR: Invalid option. Type 'psmd.sh -h' for help" >&2
-      usage
+      exit 1
       ;;
     :)
 	  echo ""
 	  echo "ERROR: Option -$OPTARG requires an argument. Type 'psmd.sh -h' for help" >&2
-	  Echo ""
+	  echo ""
       exit 1
       ;;
   esac
 done
 
 # Check option combinations
-if [ -n "${dtiraw}" -o -n "${bval}" -o -n  "${bvec}" ]; then
+if [ -n "${dtiraw}" -o -n "${bval}" -o -n "${bvec}" ]; then
     pipeline=unprocessed
-	[ -z "${dtiraw}" -o -z "${bval}" -o -z  "${bvec}" ] && { echo ""; echo "ERROR: When using raw DTI data, all options (-d -b -r) are required. Type 'psmd.sh -h' for help.";echo ""; exit 1; }
+	[ -z "${dtiraw}" -o -z "${bval}" -o -z "${bvec}" ] && { echo ""; echo "ERROR: When using raw DWI data, all options (-d -b -r) are required. Type 'psmd.sh -h' for help.";echo ""; exit 1; }
 fi
 if [ -n "${faimage}" -o -n "${mdimage}" ]; then
     pipeline=processed
 	[ -z "${faimage}" -o -z "${mdimage}" ] && { echo ""; echo "ERROR: When using processed DTI data, both options (-f and -m) are required. Type 'psmd.sh -h' for help.";echo ""; exit 1; }
 fi
+if [ ${enhmask} == true ] && [ ${pipeline} == processed ];then
+echo ""; echo "ERROR: Unprocessed DWI data needed for enhanced masking.";echo ""; exit 1
+fi
 
 # Check for skeleton_mask
-[ -z $mask ] && { echo ""; echo "ERROR: Skeleton_mask (option -s) not defined. This is mandatory! Type 'psmd.sh -h' for help";echo ""; exit 1; }
+[ -z "$mask" ] && { echo ""; echo "ERROR: Skeleton_mask (option -s) not defined. This is mandatory! Type 'psmd.sh -h' for help";echo ""; exit 1; }
 
 # Check for previous script run, which might interfere
-[ -r psmdtemp ] && { echo ""; echo "ERROR: 'psmdtemp' folder in current directory. Delete before running this script!";echo ""; exit 1; }
+if [ -r psmdtemp ];then
+[ ${cleartemp} == false ] && { echo ""; echo "ERROR: 'psmdtemp' folder in current directory. Delete before running this script!";echo ""; exit 1; }
+[ ${cleartemp} == true  ] && { rm -r psmdtemp; }
+fi
 
 # Set reporting level from options
-[ ${verbose} == true ] && { silent=false; echo "Reporting level: Verbose (all status and error messages are displayed)"; }
-[ ${silent} == false ] && { echo "";echo "PSMD processing pipeline, v 0.95 (first release)"; } 
+[ ${silent} == false ] && { echo "";echo "${metric} processing pipeline, v1.5 (2019)"; } 
+[ ${verbose} == true ] && { silent=false;echo "";echo "Reporting level: Verbose (all status and error messages are displayed)"; }
 
 redirect_cmd mkdir psmdtemp
-cd psmdtemp
+cd psmdtemp || exit 1
 
-# Raw DTI pipeline
+# Raw DWI pipeline
 if [ ${pipeline} == unprocessed ];then
-[ ${silent} == false ] && echo "Pipeline for unprocessed DTI"
-[ ${silent} == false ] && echo "...Eddy-correcting DTI data (this step will take a few minutes)"
-redirect_cmd eddy_correct ${dtirawfile} data 1
-[ ${silent} == false ] && echo "...Running brain extraction"
-redirect_cmd bet data nodiff_brain_F -F -m
+[ ${silent} == false ] && echo "Pipeline for unprocessed DWI data"
+[ ${silent} == false ] && echo "...Eddy-correcting DWI data (this step will take a few minutes)"
+redirect_cmd eddy_correct "${dtirawfile}" data 1
+[ ${silent} == false ] && echo "...Running brain extraction on b=0"
+redirect_cmd select_dwi_vols data "${bvalfile}" nodiff 0 -m
+redirect_cmd bet nodiff nodiff_brain_F -F -m
 [ ${silent} == false ] && echo "...Running tensor calculation"
-redirect_cmd dtifit -k data -o temp-DTI -m nodiff_brain_F_mask -r ${bvecfile} -b ${bvalfile}
+redirect_cmd dtifit -k data -o temp-DTI -m nodiff_brain_F_mask -r "${bvecfile}" -b "${bvalfile}"
 faimagefile=$(get_abs_filename temp-DTI_FA.nii.gz)
 mdimagefile=$(get_abs_filename temp-DTI_MD.nii.gz)
 fi
 
+# Optional (-e): Enhanced masking (new in version 1.5)
+if [ ${enhmask} == true ];then
+	[ ${silent} == false ] && echo "...Enhanced masking: Calculation trace image from shells with b-value ${enhmasb}"
+	redirect_cmd select_dwi_vols data "${bvalfile}" trace "${enhmasb}" -m
+	redirect_cmd fslmaths trace -mas nodiff_brain_F_mask trace_brain
+
+	[ ${silent} == false ] && echo "...Bias correction and tissue segmentation"
+	redirect_cmd select_dwi_vols data "${bvalfile}" meanb0 0 -m
+	redirect_cmd fslmaths meanb0 -mas nodiff_brain_F_mask meanb0_brain
+	redirect_cmd fast -t 2 -b -B -p meanb0_brain
+	redirect_cmd fslmaths meanb0_brain_prob_0 -thr 0.5 -bin seg_meanb0
+	redirect_cmd fslmaths trace.nii.gz -div meanb0_brain_bias.nii.gz -mas nodiff_brain_F_mask.nii.gz trace_unbiased.nii.gz
+	redirect_cmd fast -n 2 -N -p trace_unbiased
+	redirect_cmd fslmaths trace_unbiased_prob_0 -thr 0.3 -bin seg_trace
+	redirect_cmd fslmaths trace_unbiased -mas seg_trace trace_unbiased_seg
+	redirect_cmd fslmaths seg_meanb0 -add seg_trace -thr 2 -bin -fillh enhmask
+fi
+
 redirect_cmd mkdir tbss
-redirect_cmd cp ${faimagefile} tbss/
-redirect_cmd cd tbss
+redirect_cmd cp "${faimagefile}" tbss/
+redirect_cmd cd tbss 
 tbssfile=$(ls)
 
-# Processed DTI pipeline
-# TBSS
-[ ${pipeline} == processed -a ${silent} == false ] && echo "Calculating PSMD from already processed DTI"
+# TBSS on FA
+[ ${pipeline} == processed -a ${silent} == false ] && echo "Calculating ${metric} from already processed DTI"
 [ ${silent} == false ] && echo "...Skeletonizing FA image (this step will take a few minutes)"
-redirect_cmd tbss_1_preproc ${tbssfile}
+redirect_cmd tbss_1_preproc "${tbssfile}"
 redirect_cmd tbss_2_reg -T
 redirect_cmd tbss_3_postreg -T
 redirect_cmd tbss_4_prestats 0.2
 
+# TBSS on MD
 [ ${silent} == false ] && echo "...Projecting MD image"
 newname=$(ls origdata/)
 redirect_cmd mkdir MD 
-redirect_cmd cp ${mdimagefile} MD/${newname}
+redirect_cmd cp "${mdimagefile}" MD/"${newname}"
 redirect_cmd tbss_non_FA MD
 
+finalmask=${maskfile}
+
+# Optional (-e): TBSS on trace and enhanced mask (new in version 1.5)
+if [ ${enhmask} == true ];then
+	[ ${silent} == false ] && echo "...Projecting enhanced mask"
+	redirect_cmd mkdir trace 
+	redirect_cmd cp ../trace_unbiased.nii.gz trace/"${newname}"
+	redirect_cmd tbss_non_FA trace
+	redirect_cmd mkdir mask 
+	redirect_cmd cp ../enhmask.nii.gz mask/"${newname}"
+	redirect_cmd tbss_non_FA mask
+	redirect_cmd fslmaths stats/all_mask_skeletonised -thr 0.05 -bin stats/all_mask_skeletonised_bin.nii.gz
+	redirect_cmd fslmaths "${maskfile}" -sub stats/all_mask_skeletonised_bin.nii.gz -bin stats/skeleton_enhanced_bin.nii.gz
+	finalmask=$(get_abs_filename stats/skeleton_enhanced_bin.nii.gz)
+fi
+
+# Optional (-l): TBSS on lesion_mask (new in version 1.5)
+if [ ${lesionmasking} == true ];then
+	redirect_cmd mkdir lesionmask
+	redirect_cmd cp "${lesionmaskfile}" lesionmask/"${newname}"
+	redirect_cmd tbss_non_FA lesionmask
+	redirect_cmd fslmaths stats/all_lesionmask_skeletonised -thr 0.05 -bin stats/all_lesionmask_skeletonised_bin.nii.gz
+	redirect_cmd fslmaths "${maskfile}" -sub stats/all_lesionmask_skeletonised_bin.nii.gz -bin stats/skeleton_lesionmask_bin.nii.gz
+	if [ ${enhmask} == true ];then
+		fslmaths stats/skeleton_enhanced_bin.nii.gz -mul stats/skeleton_lesionmask_bin.nii.gz -bin stats/skeleton_combined_bin.nii.gz
+		finalmask=$(get_abs_filename stats/skeleton_combined_bin.nii.gz)
+	else
+		finalmask=$(get_abs_filename stats/skeleton_lesionmask_bin.nii.gz)
+	fi
+fi
+
 # Histogram analysis
-[ ${silent} == false ] && echo "...Histogram analysis"
-redirect_cmd fslmaths stats/all_MD_skeletonised.nii.gz -mas ${maskfile} -mul 1000000 temp_skel_mask.nii.gz
-a=$(fslstats temp_skel_mask.nii.gz -P 95)
-b=$(fslstats temp_skel_mask.nii.gz -P 5)
-psmd=$(echo - | awk "{print ( ${a} - ${b} ) / 1000000 }" | sed 's/,/./')
+redirect_cmd fslmaths stats/all_MD_skeletonised.nii.gz -mas "${finalmask}" -mul 1000000 MD_skeletonized_masked.nii.gz
+if [ ${metric} == PSMD ];then
+	[ ${silent} == false ] && echo "...Histogram analysis"
+	a=$(fslstats MD_skeletonized_masked.nii.gz -P 95)
+	b=$(fslstats MD_skeletonized_masked.nii.gz -P 5)
+	psmd=$(echo - | awk "{print ( ${a} - ${b} ) / 1000000 }" | sed 's/,/./')
+	[ ${silent} == false ] && { echo ""; echo "${metric} is ${psmd}"; echo ""; }
+	[ ${silent} == true ] && echo "${psmd}"
+fi
 
-[ ${silent} == false ] && { echo ""; echo "PSMD is ${psmd}"; echo ""; }
-[ ${silent} == true ] && echo ${psmd}
+# Mean skeletonized mean diffusivity, MSMD (new in version 1.5)
+if [ ${metric} == MSMD ];then
+	[ ${silent} == false ] && echo "...Skeleton analysis"
+	a=$(fslstats MD_skeletonized_masked.nii.gz -M)
+	msmd=$(echo - | awk "{print ${a} / 1000000 }" | sed 's/,/./')
+	[ ${silent} == false ] && { echo ""; echo "${metric} is ${msmd}"; echo ""; }
+	[ ${silent} == true ] && echo "${msmd}"
+fi
 
-cd ${basedir}
+cd "${basedir}" || exit 1
 [ ${debug} == false ] && rm -r psmdtemp
+exit 0

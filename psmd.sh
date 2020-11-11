3c427f466f39bda0cc934d16224fed94c512562a
Marco Düring <74188624+marcoduering@users.noreply.github.com>
2020-11-11 09:13:58 +0100

    v1.7

--- psmd.sh
+++ psmd.sh
@@ -2,7 +2,7 @@
 #
 # FOR MORE INFORMATION, PLEASE VISIT: http://www.psmd-marker.com
 #
-# PSMD processing pipeline, v1.6_RC2  (2020-11)
+# PSMD processing pipeline, v1.7 (2020-11)
 #
 # This script is provided under the revised BSD (3-clause) license
 # 
@@ -50,7 +50,7 @@
 usage(){
 cat <<USAGE
 
-PSMD - Peak width of Skeletonized Mean Diffusivity - pipeline version 1.6_RC2 (2020)
+PSMD - Peak width of Skeletonized Mean Diffusivity - pipeline version 1.7 (2020)
 http://www.psmd-marker.com
 
 Usage:
@@ -272,7 +272,7 @@ if [ -r psmdtemp ];then
 fi
 
 # Set reporting level from options
-[ ${silent} == false ] && { echo "";echo "${metric} processing pipeline, v1.6_RC2"; } 
+[ ${silent} == false ] && { echo "";echo "${metric} processing pipeline, v1.7"; } 
 [ ${verbose} == true ] && { silent=false;echo "";echo "Reporting level: Verbose (all status and error messages are displayed)"; }
 
 redirect_cmd mkdir psmdtemp
